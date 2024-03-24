6729ddd3ae3a34065b371481b69dd1a5b118d356
Juan Esteban Salazar <juanesteban.salazar1@utp.edu.co>
2024-03-24 00:42:39 -0500

    Inicios de portafolio esquema de la información, sobre mi , yo y mis proyectos (Poratafolio)

--- Angular/Portafolio/src/app/app.module.ts
+++ Angular/Portafolio/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortafolioComponent } from './Componentes/portafolio/portafolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortafolioComponent
   ],
   imports: [
     BrowserModule,
